64da6f19dbe42400d3fd128ce4a31c3c83b11747
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-11 17:46:03 +0400

    first commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,34 @@
-let bottom=document.querySelector(".botom")
+
+function showData(){
+    let bottom=document.querySelector(".botom");
 fetch(`http://localhost:3000/card`)
 .then(response=>response.json())
-.then(data=>
-    console.log(data)
-  
-
+.then(data=>{
+    data.foreach(card=>{
+    const card1 = document.createElement('div');
+card1.classList.add('card');
+    card.innerHTML+=`
     
+    <div class="img">
+    <img src="${card.image}" alt="" />
+  </div>
+  <div class="text">
+    <h5>${card.header}</h5>
+    <p>${card.son}</p>
+  
+      <a href="./dataesehtml/navfoot.html?id=${card.id}"> <button>VIEW MORE  </button></a>
+  
+    <button onclick="deleterobot(${card.id})">Delete</button>  
+
+    <button onclick="editRobot(${card.id})">Uptade</button> 
+  </div>
+`;
+  
+bottom.appendChild(card1)
+
+})
+}
     
     )
+}
+showData()
